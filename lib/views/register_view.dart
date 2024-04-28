6be6c1ae0e524cafd16afd585e5e727c2d3ee280
VidytBhudolia <vidytbhudlia2005@gmail.com>
2024-04-28 16:20:14 +0530

    Part 2 of sholving Shreyas's messup

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:mynotes/firebase_options.dart';
 
 class RegistrationView extends StatefulWidget {
   const RegistrationView({super.key});
@@ -50,9 +48,7 @@ class _RegistrationViewState extends State<RegistrationView> {
           ),
         ),
         child: Center(
-          
           child: Padding(
-            
             padding: const EdgeInsets.all(20.0),
             child: Container(
               width: 300,
@@ -122,19 +118,28 @@ class _RegistrationViewState extends State<RegistrationView> {
                       final email = _email.text;
                       final password = _password.text;
                       try {
-                        final userCredential = await FirebaseAuth.instance
+                        await FirebaseAuth.instance
                             .createUserWithEmailAndPassword(
                                 email: email, password: password);
-                        print(userCredential);
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            '/verify_email/', (_) => false);
                       } on FirebaseAuthException catch (e) {
                         if (e.code == 'weak-password') {
-                          print('The password provided is too weak.');
+                          if (kDebugMode) {
+                            print('The password provided is too weak.');
+                          }
                         } else if (e.code == 'email-already-in-use') {
-                          print('The account already exists for that email.');
+                          if (kDebugMode) {
+                            print('The account already exists for that email.');
+                          }
                         } else if (e.code == 'invalid-email') {
-                          print('The email address is badly formatted.');
+                          if (kDebugMode) {
+                            print('The email address is badly formatted.');
+                          }
                         } else {
-                          print(e.code);
+                          if (kDebugMode) {
+                            print(e.code);
+                          }
                         }
                       }
                     },
