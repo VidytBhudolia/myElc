529cc84744f697f09597451acae5b2669cea2925
VidytBhudolia <vidytbhudlia2005@gmail.com>
2024-04-28 16:23:12 +0530

    Part 3 of fixing it

--- lib/views/login_views.dart
+++ lib/views/login_views.dart
@@ -36,98 +36,112 @@ class _LoginViewState extends State<LoginView> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            width: 300,
-            height: 300, // Adjust the width as needed
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            // image: AssetImage("assets/background_image.png"),
+            image: AssetImage("assets/background_image_watermark.png"),
+
+            // image: AssetImage("assets/bg_black.png"),
+
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
             padding: const EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 30),
-                TextField(
-                  controller: _email,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: const InputDecoration(hintText: "Email "),
-                ),
-                const SizedBox(height: 20), // Adjusted height
-                TextField(
-                  controller: _password,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  obscureText: true,
-                  decoration: const InputDecoration(hintText: "Password "),
-                ),
-                const SizedBox(height: 20),
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                          '/register/', (route) => false);
-                    },
-                    child: const Text('Not Registered? Register Here! '),
+            child: Container(
+              width: 300,
+              height: 300, // Adjust the width as needed
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
                   ),
-                ),
-                const SizedBox(height: 10),
-                ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.green[500]!),
-                      overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                        (Set<MaterialState> states) {
-                          if (states.contains(MaterialState.hovered)) {
-                            return Colors.blue.withOpacity(0.04);
-                          }
-                          if (states.contains(MaterialState.focused) ||
-                              states.contains(MaterialState.pressed)) {
-                            return Colors.blue.withOpacity(0.12);
-                          }
-                          return null; // Defer to the widget's default.
-                        },
-                      ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const SizedBox(height: 30),
+                  TextField(
+                    controller: _email,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(hintText: "Email "),
+                  ),
+                  const SizedBox(height: 20), // Adjusted height
+                  TextField(
+                    controller: _password,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    obscureText: true,
+                    decoration: const InputDecoration(hintText: "Password "),
+                  ),
+                  const SizedBox(height: 20),
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                            '/register/', (route) => false);
+                      },
+                      child: const Text('Not Registered? Register Here! '),
                     ),
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                                email: email, password: password);
-                        if (userCredential.user!.emailVerified) {
-                          // Navigator.of(context)
-                          //     .pushNamedAndRemoveUntil('/notes/', (_) => false);
-                          Navigator.of(context)
-                              .pushNamedAndRemoveUntil('/home/', (_) => false);
-                        } else {
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                              '/verify_email/', (_) => false);
+                  ),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.green[500]!),
+                        overlayColor: MaterialStateProperty.resolveWith<Color?>(
+                          (Set<MaterialState> states) {
+                            if (states.contains(MaterialState.hovered)) {
+                              return Colors.blue.withOpacity(0.04);
+                            }
+                            if (states.contains(MaterialState.focused) ||
+                                states.contains(MaterialState.pressed)) {
+                              return Colors.blue.withOpacity(0.12);
+                            }
+                            return null; // Defer to the widget's default.
+                          },
+                        ),
+                      ),
+                      onPressed: () async {
+                        final email = _email.text;
+                        final password = _password.text;
+                        try {
+                          final userCredential = await FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                  email: email, password: password);
+                          if (userCredential.user!.emailVerified) {
+                            // Navigator.of(context)
+                            //     .pushNamedAndRemoveUntil('/notes/', (_) => false);
+                            Navigator.of(context).pushNamedAndRemoveUntil(
+                                '/home/', (_) => false);
+                          } else {
+                            Navigator.of(context).pushNamedAndRemoveUntil(
+                                '/verify_email/', (_) => false);
+                          }
+                        } on FirebaseAuthException catch (e) {
+                          if (kDebugMode) {
+                            print(e.code);
+                          }
                         }
-                      } on FirebaseAuthException catch (e) {
-                        if (kDebugMode) {
-                          print(e.code);
-                        }
-                      }
-                    },
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(color: Colors.black, fontSize: 15.0),
-                    )),
-              ],
+                      },
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(color: Colors.black, fontSize: 15.0),
+                      )),
+                ],
+              ),
             ),
           ),
         ),
